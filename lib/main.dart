e2ed8d4cf4b154b0767ec548218a872087ec2dad
daffautomo23 <148929054+daffautomo23@users.noreply.github.com>
2023-12-31 03:35:05 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,589 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+
+void main() {
+  runApp(MyApp());
+}
+
+class DatabaseHelper {
+  static Future<Database> getDatabase() async {
+    final dbPath = await getDatabasesPath();
+    return openDatabase(
+      path.join(dbPath, 'knowledge_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE knowledge(id TEXT PRIMARY KEY, title TEXT, content TEXT, category TEXT)',
+        );
+      },
+      version: 1,
+    );
+  }
+
+  static Future<void> insertKnowledge(Knowledge knowledge) async {
+    final db = await DatabaseHelper.getDatabase();
+    await db.insert(
+      'knowledge',
+      knowledge.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Knowledge>> getFavorites() async {
+    final db = await DatabaseHelper.getDatabase();
+    final List<Map<String, dynamic>> maps = await db.query('knowledge');
+
+    return List.generate(maps.length, (i) {
+      return Knowledge(
+        id: maps[i]['id'],
+        title: maps[i]['title'],
+        content: maps[i]['content'],
+        category : maps[i]['category'],
+      );
+    });
+  }
+
+  static Future<void> deleteKnowledge(String id) async {
+    final db = await DatabaseHelper.getDatabase();
+    await db.delete(
+      'knowledge',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadTheme();
+  }
+
+  void loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    });
+  }
+
+  void toggleTheme(bool value) {
+    setState(() {
+      isDarkMode = value;
+      SharedPreferences.getInstance().then((prefs) {
+        prefs.setBool('isDarkMode', value);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Knowledge Stash',
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: MainScreen(
+        toggleTheme: toggleTheme,
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  final Function(bool) toggleTheme;
+
+  MainScreen({required this.toggleTheme});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  late List<Widget> screens;
+
+  @override
+  void initState() {
+    super.initState();
+    screens = [
+      HomeFeedPage(),
+      SettingsPage(toggleTheme: widget.toggleTheme),
+      FavoritesPage(), // Tambahkan FavoritesPage di sini
+      CategoryPage()
+    ];
+  }
+
+  void navigateToAddKnowledge() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddKnowledgePage()));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Feed',
+            backgroundColor: Colors.deepPurple, // Violet background color
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.deepPurple, // Violet background color
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+            backgroundColor: Colors.deepPurple, // Violet background color
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Category',
+            backgroundColor: Colors.deepPurple, // Violet background color
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+      floatingActionButton: _currentIndex == 0 ? FloatingActionButton(
+        onPressed: navigateToAddKnowledge,
+        child: Icon(Icons.add),
+      ) : null,
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  final Function(bool) toggleTheme;
+
+  SettingsPage({required this.toggleTheme});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pengaturan'),
+      ),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: Text('Dark Mode'),
+            value: Theme.of(context).brightness == Brightness.dark,
+            onChanged: (bool value) {
+              toggleTheme(value);
+            },
+          ),
+          // Add more settings here
+        ],
+      ),
+    );
+  }
+}
+
+class HomeFeedPage extends StatefulWidget {
+  @override
+  _HomeFeedPageState createState() => _HomeFeedPageState();
+}
+
+class _HomeFeedPageState extends State<HomeFeedPage> {
+  Future<List<Knowledge>> fetchKnowledge() async {
+    final response = await http.get(Uri.parse('https://65811d473dfdd1b11c42733b.mockapi.io/tubes/knowledge'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> values = json.decode(response.body);
+      return values.map((e) => Knowledge.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load knowledge');
+    }
+  }
+
+  void deleteKnowledge(String id) async {
+    var response = await http.delete(
+      Uri.parse('https://65811d473dfdd1b11c42733b.mockapi.io/tubes/knowledge/$id'),
+    );
+
+    if (response.statusCode == 200) {
+      // Tampilkan pesan sukses
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Knowledge berhasil dihapus')),
+      );
+
+      // Muat ulang daftar knowledge
+      setState(() {});
+    } else {
+      // Tampilkan pesan error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Gagal menghapus knowledge')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Knowledge Stash'),
+      ),
+      body: FutureBuilder<List<Knowledge>>(
+        future: fetchKnowledge(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return KnowledgeCard(
+                  knowledge: snapshot.data![index],
+                  onDelete: deleteKnowledge,
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class Knowledge {
+  final String id;
+  final String title;
+  final String content;
+  final String category;
+
+  Knowledge({required this.id, required this.title, required this.content, required this.category});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'category': category,
+    };
+  }
+
+  factory Knowledge.fromJson(Map<String, dynamic> json) {
+    return Knowledge(
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+      category: json['category'],
+    );
+  }
+}
+
+class KnowledgeCard extends StatelessWidget {
+  final Knowledge knowledge;
+  final Function(String) onDelete;
+
+  KnowledgeCard({required this.knowledge, required this.onDelete});
+
+  void saveKnowledge(BuildContext context) async {
+    await DatabaseHelper.insertKnowledge(knowledge);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Knowledge berhasil disimpan')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              knowledge.title,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              knowledge.category,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(knowledge.content),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  onPressed: () => saveKnowledge(context),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete, color: Colors.red),
+                  onPressed: () => onDelete(knowledge.id),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddKnowledgePage extends StatefulWidget {
+  @override
+  _AddKnowledgePageState createState() => _AddKnowledgePageState();
+}
+
+class _AddKnowledgePageState extends State<AddKnowledgePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+  final _categoryController = TextEditingController(); // Tambahkan controller untuk kategori
+
+  Future<void> submitKnowledge() async {
+    final String title = _titleController.text;
+    final String content = _contentController.text;
+    final String category = _categoryController.text; // Dapatkan input kategori
+    final String createdAt = DateTime.now().toIso8601String();
+
+    var response = await http.post(
+      Uri.parse('https://65811d473dfdd1b11c42733b.mockapi.io/tubes/knowledge'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'createdAt': createdAt,
+        'title': title,
+        'content': content,
+        'category': category, // Sertakan kategori dalam body
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      Navigator.of(context).pop();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Berhasil menambahkan knowledge')),
+      );
+    } else {
+      Navigator.of(context).pop();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Gagal menambahkan knowledge')),
+      );
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Knowledge'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextFormField(
+                controller: _titleController,
+                decoration: InputDecoration(labelText: 'Title'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _contentController,
+                decoration: InputDecoration(labelText: 'Content'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter content';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(controller: _categoryController,
+                decoration: InputDecoration(labelText: 'Category'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter category';
+                  }
+                  return null;
+                },),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    submitKnowledge();
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+}
+
+class FavoritesPage extends StatefulWidget {
+  @override
+  _FavoritesPageState createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  late Future<List<Knowledge>> favoriteKnowledge;
+
+  @override
+  void initState() {
+    super.initState();
+    loadFavorites();
+  }
+
+  Future<void> loadFavorites() async {
+    setState(() {
+      favoriteKnowledge = DatabaseHelper.getFavorites();
+    });
+  }
+
+  void deleteKnowledge(String id) async {
+    await DatabaseHelper.deleteKnowledge(id);
+    loadFavorites();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorites'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: loadFavorites,
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<Knowledge>>(
+        future: favoriteKnowledge,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No favorites added'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final knowledge = snapshot.data![index];
+                return Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text(
+                      knowledge.title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Padding(
+                      padding: EdgeInsets.only(top: 8.0),
+                      child: Text(knowledge.content),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => deleteKnowledge(knowledge.id),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class CategoryPage extends StatefulWidget {
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  final List<String> categories = [
+    'Coding',
+    'Flutter',
+    'Crypto',
+    'Blokchain',
+    'Machine Learning',
+    'Deep Learning'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Category'),
+      ),
+      body: ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(categories[index]),
+            onTap: () {
+              // Handle category tap
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+class PlaceholderWidget extends StatelessWidget {
+  final Color color;
+
+  PlaceholderWidget(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+    );
+  }
+}
